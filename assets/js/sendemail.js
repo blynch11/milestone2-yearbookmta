be33f0beee9efad3a102fa6c5484c457b1acbae3
blynch11 <blynch2491@gmail.com>
2020-11-03 19:19:44 +0000

    attempted to add email. attempted to fix maps.

---
+++ assets/js/sendemail.js
@@ -0,0 +1,10 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail", "brendan", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "your_message": contactForm.message.value
+    })
+     .then(alert("Thank you, your message has been received and we will get back to you soon!"));
+
+  $("#contact-form").text();
+}
