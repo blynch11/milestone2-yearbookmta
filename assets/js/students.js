043cf4a1415b4199071b2dfbd13a1cfab46ef07c
blynch11 <blynch2491@gmail.com>
2020-11-07 11:03:53 +0000

    added java to hide conten using id

--- assets/js/students.js
+++ assets/js/students.js
@@ -1,8 +1,30 @@
-$(document).ready(function () {
-    
-    $(".read-more").on('click', function(){
-        $(this).parent().toggleClass("showContent");
 
+    $(document).ready(function(){
+        $("#quote").css("overflow", "hidden");
+        $("#quote-1").css("overflow", "hidden");
+        $("#quote-2").css("overflow", "hidden");
+        $("#quote-3").css("overflow", "hidden");
+        $("#quote-4").css("overflow", "hidden");
+        $("#quote-5").css("overflow", "hidden");
+        $("#quote-6").css("overflow", "hidden");
+        $("#quote-7").css("overflow", "hidden");
+
+     
     });
-}
+
+   var button = document.getElementsByClassName("read-more");
+   var content = document.getElementsByClassName("show-content");
+
+   button.onclick = function(){
+       
+
+   }
+
+   
+
+
+    
+    
+    
+
 
