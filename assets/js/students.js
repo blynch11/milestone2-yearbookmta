43818797ebfe16608ffbb6e9ba1a57344603debb
blynch11 <blynch2491@gmail.com>
2020-11-10 14:15:16 +0000

    added the expanding paragraph in students.js

--- assets/js/students.js
+++ assets/js/students.js
@@ -1,26 +1,16 @@
 
     $(document).ready(function(){
-        $("#quote").css("overflow", "hidden");
-        $("#quote-1").css("overflow", "hidden");
-        $("#quote-2").css("overflow", "hidden");
-        $("#quote-3").css("overflow", "hidden");
-        $("#quote-4").css("overflow", "hidden");
-        $("#quote-5").css("overflow", "hidden");
-        $("#quote-6").css("overflow", "hidden");
-        $("#quote-7").css("overflow", "hidden");
-
+        $("p").css("display", "none");
+        
      
     });
 
 
-    //
+   
 
-   var button = document.getElementsByClassName("read-more");
-   var content = document.getElementsByClassName("show-content");
-//
-
-
-  
+$(".btn").click(function(){
+    $(".btn").prev().css("display", "block")
+});
    
 
 
