61c6d53882faeb34619283c86c2eed06cb6970da
blynch11 <blynch2491@gmail.com>
2020-11-10 15:01:11 +0000

    added jump-to links, tidied up expand

--- assets/js/students.js
+++ assets/js/students.js
@@ -6,16 +6,63 @@
     });
 
 
-   
+// button click events   
 
-$(".btn").click(function(){
-    $(".btn").prev().css("display", "block")
+$(".read-more").click(function(){
+    $("#quote").css("display", "block");
+});
+
+
+$(".read-more1").click(function(){
+    $("#quote1").css("display", "block");
+});
+
+$(".read-more2").click(function(){
+    $("#quote2").css("display", "block");
+});
+
+$(".read-more3").click(function(){
+    $("#quote3").css("display", "block");
+});
+
+$(".read-more4").click(function(){
+    $("#quote4").css("display", "block");
+});
+
+$(".read-more5").click(function(){
+    $("#quote5").css("display", "block");
+});
+
+$(".read-more6").click(function(){
+    $("#quote6").css("display", "block");
+});
+
+$(".read-more7").click(function(){
+    $("#quote7").css("display", "block");
 });
    
+$(".read-more8").click(function(){
+    $("#quote8").css("display", "block");
+});
+
+$(".read-more9").click(function(){
+    $("#quote9").css("display", "block");
+});
+
+$(".read-more10").click(function(){
+    $("#quote10").css("display", "block");
+});
+
+$(".read-more11").click(function(){
+    $("#quote11").css("display", "block");
+});
 
 
     
-    
+// button toggle to read-less
+
+
+
     
 
 
