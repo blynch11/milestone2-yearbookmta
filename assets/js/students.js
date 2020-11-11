030b5498fe451e52252f4b61a0dbca41baeb53a8
blynch11 <blynch2491@gmail.com>
2020-11-11 17:15:50 +0000

    added correct functions to expand/collapse

--- assets/js/students.js
+++ assets/js/students.js
@@ -8,54 +8,155 @@
 
 // button click events   
 
-$(".read-more").click(function(){
-    $("#quote").css("display", "block");
+
+
+$("#read-more").click(function(){
+    if ($(this).text() === 'Read More'){
+        $(this).text('Read less');
+        $("#quote").css("display", "block").toggle(slow);
+    }
+    else{
+($("#read-more").text() === 'Read Less' )
+        $(this).text('Read More');
+        $("#quote").css("display", "none").toggle(slow);
+    }
+});
+
+$("#read-more1").click(function(){
+    if ($(this).text() === 'Read More'){
+        $(this).text('Read less');
+        $("#quote1").css("display", "block");
+    }
+    else{
+($("#read-more1").text() === 'Read Less' )
+        $(this).text('Read More');
+        $("#quote1").css("display", "none");
+    }
+});
+
+$("#read-more2").click(function(){
+    if ($(this).text() === 'Read More'){
+        $(this).text('Read less');
+        $("#quote2").css("display", "block");
+    }
+    else{
+($("#read-more2").text() === 'Read Less' )
+        $(this).text('Read More');
+        $("#quote2").css("display", "none");
+    }
+});
+
+$("#read-more3").click(function(){
+    if ($(this).text() === 'Read More'){
+        $(this).text('Read less');
+        $("#quote3").css("display", "block");
+    }
+    else{
+($("#read-more3").text() === 'Read Less' )
+        $(this).text('Read More');
+        $("#quote3").css("display", "none");
+    }
+});
+
+$("#read-more4").click(function(){
+    if ($(this).text() === 'Read More'){
+        $(this).text('Read less');
+        $("#quote4").css("display", "block");
+    }
+    else{
+($("#read-more4").text() === 'Read Less' )
+        $(this).text('Read More');
+        $("#quote4").css("display", "none");
+    }
+});
+
+$("#read-more5").click(function(){
+    if ($(this).text() === 'Read More'){
+        $(this).text('Read less');
+        $("#quote5").css("display", "block");
+    }
+    else{
+($("#read-more5").text() === 'Read Less' )
+        $(this).text('Read More');
+        $("#quote5").css("display", "none");
+    }
+});
+
+$("#read-more6").click(function(){
+    if ($(this).text() === 'Read More'){
+        $(this).text('Read less');
+        $("#quote6").css("display", "block");
+    }
+    else{
+($("#read-more6").text() === 'Read Less' )
+        $(this).text('Read More');
+        $("#quote6").css("display", "none");
+    }
+});
+
+$("#read-more7").click(function(){
+    if ($(this).text() === 'Read More'){
+        $(this).text('Read less');
+        $("#quote7").css("display", "block");
+    }
+    else{
+($("#read-more7").text() === 'Read Less' )
+        $(this).text('Read More');
+        $("#quote7").css("display", "none");
+    }
+});
+
+$("#read-more8").click(function(){
+    if ($(this).text() === 'Read More'){
+        $(this).text('Read less');
+        $("#quote8").css("display", "block");
+    }
+    else{
+($("#read-more8").text() === 'Read Less' )
+        $(this).text('Read More');
+        $("#quote8").css("display", "none");
+    }
+});
+
+$("#read-more9").click(function(){
+    if ($(this).text() === 'Read More'){
+        $(this).text('Read less');
+        $("#quote9").css("display", "block");
+    }
+    else{
+($("#read-more9").text() === 'Read Less' )
+        $(this).text('Read More');
+        $("#quote9").css("display", "none");
+    }
+});
+
+$("#read-more10").click(function(){
+    if ($(this).text() === 'Read More'){
+        $(this).text('Read less');
+        $("#quote10").css("display", "block");
+    }
+    else{
+($("#read-more10").text() === 'Read Less' )
+        $(this).text('Read More');
+        $("#quote10").css("display", "none");
+    }
+});
+
+$("#read-more11").click(function(){
+    if ($(this).text() === 'Read More'){
+        $(this).text('Read less');
+        $("#quote11").css("display", "block");
+    }
+    else{
+($("#read-more11").text() === 'Read Less' )
+        $(this).text('Read More');
+        $("#quote11").css("display", "none");
+    }
 });
 
 
-$(".read-more1").click(function(){
-    $("#quote1").css("display", "block");
-});
 
-$(".read-more2").click(function(){
-    $("#quote2").css("display", "block");
-});
-
-$(".read-more3").click(function(){
-    $("#quote3").css("display", "block");
-});
-
-$(".read-more4").click(function(){
-    $("#quote4").css("display", "block");
-});
-
-$(".read-more5").click(function(){
-    $("#quote5").css("display", "block");
-});
-
-$(".read-more6").click(function(){
-    $("#quote6").css("display", "block");
-});
-
-$(".read-more7").click(function(){
-    $("#quote7").css("display", "block");
-});
-   
-$(".read-more8").click(function(){
-    $("#quote8").css("display", "block");
-});
-
-$(".read-more9").click(function(){
-    $("#quote9").css("display", "block");
-});
-
-$(".read-more10").click(function(){
-    $("#quote10").css("display", "block");
-});
-
-$(".read-more11").click(function(){
-    $("#quote11").css("display", "block");
-});
+// button click events  
 
 
     
