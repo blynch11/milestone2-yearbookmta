eb2953aeabe8601aa332e9b1708f0fcd38295180
blynch11 <blynch2491@gmail.com>
2021-01-26 20:03:20 +0000

    tidied up the function, added comments. changed var to let

--- assets/js/students.js
+++ assets/js/students.js
@@ -1,30 +1,17 @@
-
-    $(document).ready(function(){
-        $("i").css("display", "none");
-        
-     
-    });
+$(document).ready(function(){
+    $("i").css("display", "none");    
+});
 
 // button click events
 function readChange(id, quote) {
+    //This function ...
     let textVal = document.getElementById(id).innerText;
     if (textVal === 'Read More'){
         document.getElementById(id).innerText = 'Read less';
         document.getElementById(quote).style.display = 'block';
-    }
-    else{
+    } else {
         document.getElementById(id).innerText = 'Read More';
         document.getElementById(quote).style.display = 'none';
     }
 }
-
-
-
-
-
-
-
-
-    
-
 
