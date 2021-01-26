8ddf6fa23acec247dae49f06ab2fe3076d718872
blynch11 <blynch2491@gmail.com>
2021-01-26 20:16:43 +0000

    changed var to let. tidied up code to make it more readable

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,25 +1,22 @@
- $(document).ready(function(){
-        $(".next").on('click', function(){
-            var visibleImg = $('.active');
-            var nextImg = visibleImg.next();
+//this does x and y
+$(document).ready(function(){
+        $(".next").on('click', function() {
+            let visibleImg = $('.active');
+            let nextImg = visibleImg.next();
 
-
-        if(nextImg.length){
-            visibleImg.removeClass('active').css('z-index', '-1');
-            nextImg.addClass('active').css('z-index', 1);
-        }
+            if(nextImg.length){
+                visibleImg.removeClass('active').css('z-index', '-1');
+                nextImg.addClass('active').css('z-index', 1);
+            }
         });
         
         $(".previous").on('click', function(){
-            var visibleImg = $('.active');
-            var prevImg = visibleImg.prev();
+            let visibleImg = $('.active');
+            let prevImg = visibleImg.prev();
 
-
-        if(prevImg.length){
-            visibleImg.removeClass('active').css('z-index', '-1');
-            prevImg.addClass('active').css('z-index', 1);
-        }
+            if(prevImg.length) {
+                visibleImg.removeClass('active').css('z-index', '-1');
+                prevImg.addClass('active').css('z-index', 1);
+            }
         });
-
-
-    });
+});
