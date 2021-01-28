ea6d4eea205bf4193df4dba8f83aa67ee1ce2ffc
blynch11 <blynch2491@gmail.com>
2021-01-28 17:12:26 +0000

    added new carousel to home page. Added JS to gallery.js for the carousel

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,22 +1,16 @@
-//this fucntion allows users to slide through images of the highlights of our school year
-$(document).ready(function(){
-        $(".next").on('click', function() {
-            let visibleImg = $('.active');
-            let nextImg = visibleImg.next();
-
-            if(nextImg.length){
-                visibleImg.removeClass('active').css('z-index', '-1');
-                nextImg.addClass('active').css('z-index', 1);
-            }
-        });
-        
-        $(".previous").on('click', function(){
-            let visibleImg = $('.active');
-            let prevImg = visibleImg.prev();
-
-            if(prevImg.length) {
-                visibleImg.removeClass('active').css('z-index', '-1');
-                prevImg.addClass('active').css('z-index', 1);
-            }
-        });
-});
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
